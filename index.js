8b689407ba5f54adbf3e89c3cfee377af1a67c87
DevanshParashar165 <parashardevansh165@gmail.com>
2026-01-28 23:48:49 +0530

    Security added

---
+++ index.js
@@ -0,0 +1,79 @@
+import express from "express"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import rateLimit from "express-rate-limit"
+import helmet from "helmet"
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp"
+import cookieParser from "cookie-parser"
+import cors from cors
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT
+
+//Global rate limiting
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message : "Too many request from this IP. Please try again later"
+})
+
+//security middleware
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(hpp())
+app.use('/api',limiter)
+
+//logging middleware
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan('dev'))
+}
+
+//Global error handler
+app.use((err,req,res,next)=>{
+    console.log(err.stack)
+    res.status(err.status || 500).json({
+        status : "error",
+        message : err.message || "Internal server error",
+        ...(process.env.NODE_ENV==="development " && {stack : err.stack})
+    })
+})
+
+//cors configuration
+
+app.use(cors({
+    origin : process.env.CLIENT_URL || "http://localhost:5173",
+    credential : true,
+    methods : ["GET","POST","PUT","DELETE","PATCH","HEAD","OPTIONS"],
+    allowedHeaders : [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "device-remember-token",
+        "Access-Control-Allow-Origin",
+        "Origin",
+        "Accept",
+    ]
+}))
+
+//Api Routes
+
+//Body parser middleware
+app.use(express.json({limit:'10kb'}))
+app.use(express.urlencoded({extended : true , limit : " 10kb"}))
+app.use(cookieParser())
+
+//404 handler
+app.use((req,res)=>{
+    res.status(404)
+       .json({
+        status : "Error",
+        message : "Route not found"
+       })
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listenig on port ${port} in ${process.env.NODE_ENV}`)
+})
